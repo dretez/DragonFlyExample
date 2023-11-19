d4fb7dace952bcb4c46a51f7861b9eda45ce91b9
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-19 16:23:56 -0600

    Updated Dragonfly to 0.2.0 and showed basic modded texture loading

--- src/main/java/useless/dfexample/DFExampleMod.java
+++ src/main/java/useless/dfexample/DFExampleMod.java
@@ -10,7 +10,7 @@ import turniplabs.halplibe.helper.EntityHelper;
 import useless.dfexample.entity.EntityTest;
 import useless.dfexample.entity.EntityTestModel;
 import useless.dfexample.entity.RenderTest;
-import useless.dragonfly.block.BlockModel;
+import useless.dragonfly.debug.block.BlockModel;
 import useless.dragonfly.helper.ModelHelper;
 import useless.dragonfly.model.block.BlockModelDragonFly;
 
@@ -19,34 +19,33 @@ public class DFExampleMod implements ModInitializer {
     public static final String MOD_ID = "dfexample";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final Block testBlock = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock.json")))
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock.json")))
 		.setTextures(3,1)
-		.build(new BlockModel("testblock", 1000, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock.json")));
+		.build(new BlockModel("testblock", 1000, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock.json")));
 	public static final Block testBlock2 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock2.json")))
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock2.json")))
 		.setTextures(13,13)
-		.build(new BlockModel("testblock2", 1001, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock2.json")));
+		.build(new BlockModel("testblock2", 1001, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock2.json")));
 	public static final Block testBlock3 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock3.json")))
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock3.json")))
 		.setTextures(4,0)
-		.build(new BlockModel("testblock3", 1002, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "TestBlock3.json")));
-	public static final Block testBlock4 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "dragon_egg.json")))
+		.build(new BlockModel("testblock3", 1002, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/TestBlock3.json")));
+	public static final Block modernDragonEgg = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/dragon_egg.json")))
 		.setTextures(5,2)
-		.build(new BlockModel("testblock4", 1003, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "dragon_egg.json")));
-	public static final Block testBlock5 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "beacon.json")))
+		.build(new BlockModel("exampleDragon", 1003, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/dragon_egg.json")));
+	public static final Block modernBeacon = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/beacon.json")))
 		.setTextures(1,3)
-		.build(new BlockModel("testblock5", 1004, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "beacon.json")));
-	public static final Block testBlock6 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "directionPyramid.json")))
+		.build(new BlockModel("exampleBeacon", 1004, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/beacon.json")));
+	public static final Block sideTest = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/directionPyramid.json")))
 		.setTextures(1,3)
-		.build(new BlockModel("testblock6", 1005, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "directionPyramid.json")));
-	public static final Block testBlock7 = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "stool.json")))
+		.build(new BlockModel("exampleSides", 1005, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/directionPyramid.json")));
+	public static final Block btaStool = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/stool.json")))
 		.setTextures(1,3)
-		.build(new BlockModel("testblock7", 1006, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "stool.json")));
-
+		.build(new BlockModel("exampleStool", 1006, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/stool.json")));
     @Override
     public void onInitialize() {
         LOGGER.info("DFExampleMod initialized.");
