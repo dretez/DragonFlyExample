0d63288faf803ffe7808125cd9e1a3c1f25d0934
dretez <105381869+dretez@users.noreply.github.com>
2024-08-12 14:35:31 +0100

    Updated file names for compatibility with case-sensitive systems When loading the models for the blocks added by this template, the game looks for the corresponding files in lowercase. This isn't a problem in case-insensitive systems like Windows, but does cause problems building in case-sensitive systems like Linux.

--- src/main/java/useless/dfexample/DFExampleMod.java
+++ src/main/java/useless/dfexample/DFExampleMod.java
@@ -46,8 +46,8 @@ public class DFExampleMod implements ClientStartEntrypoint, GameStartEntrypoint 
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/beacon.json")))
 		.build(new BlockModel("exampleBeacon", 1004, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/beacon.json")));
 	public static final Block sideTest = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/directionPyramid.json")))
-		.build(new BlockModel("exampleSides", 1005, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/directionPyramid.json")));
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/direction_pyramid.json")))
+		.build(new BlockModel("exampleSides", 1005, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/direction_pyramid.json")));
 	public static final Block btaStool = new BlockBuilder(MOD_ID)
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/stool.json")))
 		.build(new BlockModel("exampleStool", 1006, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/stool.json")));
